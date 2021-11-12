2acbc1f03df0ae2f4afcbf1f01d60965468874fd
tori19960105 <go@u3sys.co.jp>
2021-11-13 04:36:12 +0900

    *

--- start_ethminer.sh
+++ start_ethminer.sh
@@ -1,4 +1,4 @@
 #
 # Example shell file for starting ethminer
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./ethminer/ethminer -P stratum1+tcp://0xc4a02f2683d45dad7efb5bd29909b9e4d27eddf7.Rig001@eth-eu1.nanopool.org:9999
+./ethminer/ethminer -P stratum1+tcp://0xffeac888a46ad4d9fc6d8d0e54036af4a1a60b38.Rig001@eth-eu1.nanopool.org:9999
